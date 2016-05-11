e72c5a43717d93ca35e5a3f9a4aa9c8a396c6838
Timofey Dergachev <t@exeto.me>
2016-05-11 17:50:38 +0300

    Update brew.sh and npm.sh

--- brew.sh
+++ brew.sh
@@ -4,23 +4,26 @@
 brew update
 
 # Install more recent versions of some OS X tools.
-brew install python
-brew install python3
-brew install ruby
+brew install \
+  python \
+  python3 \
+  ruby
 
 # Install other useful binaries.
-brew install node
-brew install git
-brew install docker
-brew install docker-machine
-brew install docker-compose
-brew install mc
-brew install httpie
-brew install ngrok
-brew install tree
-brew install ssh-copy-id
-brew install dos2unix
-brew install recode
+brew install \
+  node \
+  git \
+  docker \
+  docker-machine \
+  docker-compose \
+  mc \
+  httpie \
+  ngrok \
+  tree \
+  ssh-copy-id \
+  dos2unix \
+  recode \
+  caddy
 
 # Remove outdated versions from the cellar.
 brew cleanup
--- npm.sh
+++ npm.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
 # Install of useful packages.
-npm install -g a11y
-npm install -g yaspeller
-npm install -g eslint
-npm install -g babel-eslint
-npm install -g eslint-config-airbnb
-npm install -g gulp
-npm install -g n
-npm install -g npm-check-updates
-npm install -g pm2
-npm install -g sortimg
-npm install -g tldr
+npm install -g \
+  a11y \
+  yaspeller \
+  eslint \
+  eslint-config-exeto \
+  eslint-plugin-import \
+  eslint-plugin-jsx-a11y \
+  eslint-plugin-react \
+  babel-eslint \
+  stylelint \
+  stylelint-config-standard \
+  gulp \
+  n \
+  npm-check-updates \
+  pm2 \
+  sortimg \
+  tldr \
+  node-inspector \
+  ntl
