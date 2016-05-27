7cfb56179817505b3d3ef081272051db692e9f23
Timofey Dergachev <t@exeto.me>
2016-05-27 17:39:39 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ curl -fsSL https://goo.gl/k58RkX | sh
 
 export DOTFILES=$HOME/.dotfiles
 
-ln -si $DOTFILES/oh-my-zsh        $HOME/.oh-my-zsh/custom
-ln -si $DOTFILES/.editorconfig    $HOME/.editorconfig
-ln -si $DOTFILES/.gitconfig       $HOME/.gitconfig
-ln -si $DOTFILES/.gitignore       $HOME/.gitignore
-ln -si $DOTFILES/.npmrc           $HOME/.npmrc
-ln -si $DOTFILES/.zshrc           $HOME/.zshrc
-ln -si $DOTFILES/.hushlogin       $HOME/.hushlogin
+ln -sin $DOTFILES/oh-my-zsh        $HOME/.oh-my-zsh/custom
+ln -si  $DOTFILES/.editorconfig    $HOME/.editorconfig
+ln -si  $DOTFILES/.gitconfig       $HOME/.gitconfig
+ln -si  $DOTFILES/.gitignore       $HOME/.gitignore
+ln -si  $DOTFILES/.npmrc           $HOME/.npmrc
+ln -si  $DOTFILES/.zshrc           $HOME/.zshrc
+ln -si  $DOTFILES/.hushlogin       $HOME/.hushlogin
